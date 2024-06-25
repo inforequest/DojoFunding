c1b90c8369f6ea6dd258e5fa7dc7fcef0444ee74
inforequest <Kxx211d@gmail.com>
2024-06-25 11:03:54 +0100

    Update scriptslight.js

--- scriptslight.js
+++ scriptslight.js
@@ -1,15 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     var popupModal = document.getElementById('popupModal');
     var calendlyModal = document.getElementById('calendlyModal');
+    var popupModalContent = document.querySelector('#popupModal .modal-content');
     var calendlyModalContent = document.querySelector('#calendlyModal .modal-content');
-  var closeModal = document.getElementById('closeModal');
+    var continueButton = document.getElementById('continueButton');
+    var closeModal = document.getElementById('closeModal');
     var button = document.querySelector('.button');
-    var button1 = document.querySelector('.button1'); 
-    var button2 = document.querySelector('.button2');
-
+    var button1 = document.querySelector('.button1');
 
      calendlyModal.style.display = 'none';
     
+    if (popupModal && continueButton) {
+        continueButton.addEventListener('click', function() {
+            popupModalContent.classList.add('float-out');
+            popupModal.classList.add('fade-out');
+
+            // Wait for the animation to finish before hiding the modal
+            setTimeout(function() {
+                popupModal.style.display = 'none';
+            }, 250); // Match the duration of the animation
+        });
+    } else {
+        console.log('Popup Modal or continue button not found');
+    }
 
     if (calendlyModal && closeModal) {
         closeModal.addEventListener('click', function() {
@@ -45,17 +58,10 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Button1 not found');
     }
 
-      if (button2) {
-        button1.addEventListener('click', function() {
-            calendlyModal.style.display = 'block';
-            calendlyModalContent.classList.remove('float-out');
-            calendlyModal.classList.remove('fade-out');
-        });
-    } else {
-        console.log('Button2 not found');
-    }
-
     window.addEventListener('click', function(event) {
+        if (event.target === popupModal) {
+            popupModal.style.display = 'none';
+        }
         if (event.target === calendlyModal) {
             calendlyModal.style.display = 'none';
         }
